0eae45feff1e1a2c1d17e93c65562b2b318ff6f4
Ryan Tablada <ryan.tablada@gmail.com>
2013-05-01 12:37:35 -0500

    Added config to the right folder I think...

---
+++ src/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Stripe Client ID - Stripe Connect
+	|--------------------------------------------------------------------------
+	|
+	| This is the registered Client ID from your application settings
+	| https://manage.stripe.com/account/applications/settings
+	|
+	*/
+
+	'clientId' => 'CLIENT_ID',
+);
